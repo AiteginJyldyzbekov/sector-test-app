9a0bcfc8302b35fa85077610043135b5e8eabde7
Improove3567 <jyldyzbekov06aiti@gmail.com>
2023-07-23 15:05:16 +0600

    fix:bug with pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { Routing } from "./pages";
 import "./styles/App.css";
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/pages/1");
-  }, []);
 
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./home-page";
 
 export const Routing = () => {
   return (
     <Routes>
       <Route path="/pages/:id" element={<HomePage />} />
+      <Route path="*" element={<Navigate to="/pages/1" replace />} />
     </Routes>
   );
 };
